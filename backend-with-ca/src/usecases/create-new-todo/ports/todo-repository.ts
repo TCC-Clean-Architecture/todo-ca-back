7dd3871264398a9aa5c0fa7fe9194a21501a7954
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 10:34:53 -0300

    feat(repository): altera metodo findbyid permitindo retornar null

--- backend-with-ca/src/usecases/create-new-todo/ports/todo-repository.ts
+++ backend-with-ca/src/usecases/create-new-todo/ports/todo-repository.ts
@@ -3,5 +3,5 @@ import { type ITodoInserted } from '../interfaces/todo-inserted'
 
 export interface ITodoRepository {
   create: (todo: ITodo) => Promise<string>
-  findById: (todoId: string) => Promise<ITodoInserted>
+  findById: (todoId: string) => Promise<ITodoInserted | null>
 }
