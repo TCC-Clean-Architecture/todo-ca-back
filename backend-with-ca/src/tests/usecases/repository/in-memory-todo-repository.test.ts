dcc0b8e63a3de9d8c157683d04a49822088a8cb5
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 10:37:03 -0300

    feat(repository): adiciona create e findbyid no repositorio de todo

---
+++ backend-with-ca/src/tests/usecases/repository/in-memory-todo-repository.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import { type ITodo } from '../../../usecases/create-new-todo/interfaces/todo'
+import { InMemoryTodoRepository } from '../../../usecases/create-new-todo/repository/in-memory-todo-repository'
+
+describe.only('In memory todo repository testing', () => {
+  it('should create a new todo on repository', async () => {
+    const todo: ITodo = {
+      name: 'thisisname',
+      description: 'thisisdescription',
+      status: 'todo'
+    }
+    const repository = new InMemoryTodoRepository([])
+    const createdId = await repository.create(todo)
+    const result = await repository.findById(createdId)
+    expect(result).to.deep.include({
+      ...todo
+    })
+    expect(result).to.have.property('id')
+    expect(result).to.have.property('createdAt')
+    expect(result?.createdAt).to.instanceOf(Date)
+  })
+})
---
+++ backend-with-ca/src/usecases/create-new-todo/repository/in-memory-todo-repository.ts
@@ -0,0 +1,27 @@
+import { type ITodo } from '../interfaces/todo'
+import { type ITodoInserted } from '../interfaces/todo-inserted'
+import { type ITodoRepository } from '../ports/todo-repository'
+
+class InMemoryTodoRepository implements ITodoRepository {
+  public readonly repository: ITodoInserted[]
+  constructor (initialValue: ITodoInserted[]) {
+    this.repository = initialValue || []
+  }
+
+  async create (todo: ITodo): Promise<string> {
+    const id = 'test'
+    this.repository.push({
+      id,
+      ...todo,
+      createdAt: new Date()
+    })
+    return id
+  }
+
+  async findById (todoId: string): Promise<ITodoInserted | null> {
+    const todo = this.repository.find(item => item.id === todoId)
+    return todo ?? null
+  }
+}
+
+export { InMemoryTodoRepository }
