056c471c975f6f4d87c82fba7c9d2fff64d66aab
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 17:30:57 -0300

    feat(todoname): adiciona entidade de todo name

---
+++ backend-with-ca/src/entities/todo-name.ts
@@ -0,0 +1,26 @@
+import { type Either, left, right } from '../shared/either'
+import { InvalidTodoNameError } from './errors/invalid-name-error'
+
+class TodoName {
+  public readonly value: string
+  constructor (value: string) {
+    this.value = value
+  }
+
+  public static create (todoName: string): Either<InvalidTodoNameError, TodoName> {
+    if (!TodoName.validate(todoName)) {
+      return left(new InvalidTodoNameError(todoName))
+    }
+    return right(new TodoName(todoName))
+  }
+
+  public static validate (todoName: string): boolean {
+    if (todoName.length < 3 || todoName.length > 100 || (typeof todoName !== 'string')) {
+      return false
+    }
+
+    return true
+  }
+}
+
+export { TodoName }
