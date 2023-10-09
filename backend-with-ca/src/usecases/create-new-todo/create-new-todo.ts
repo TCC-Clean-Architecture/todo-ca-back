01f7f20dbee1628e073a6b6f6d9459a26ab25ef2
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 22:27:13 -0300

    feat(todo): implementa validacao tipos de erro no use case

--- backend-with-ca/src/usecases/create-new-todo/create-new-todo.ts
+++ backend-with-ca/src/usecases/create-new-todo/create-new-todo.ts
@@ -3,6 +3,9 @@ import { type ITodo } from '../../entities/interfaces/todo'
 import { type ITodoInserted } from './interfaces/todo-inserted'
 import { type ITodoRepository } from './ports/todo-repository'
 import { Todo } from '../../entities/todo'
+import { type InvalidTodoNameError } from '../../entities/errors/invalid-name-error'
+import { type InvalidTodoStatusError } from '../../entities/errors/invalid-status-error'
+import { type InvalidTodoDescriptionError } from '../../entities/errors/invalid-description-error'
 
 class CreateNewTodoUseCase {
   private readonly todoRepository: ITodoRepository
@@ -10,10 +13,10 @@ class CreateNewTodoUseCase {
     this.todoRepository = todoRepository
   }
 
-  public async execute (todo: ITodo): Promise<Either<null, ITodoInserted>> {
+  public async execute (todo: ITodo): Promise<Either<null | InvalidTodoNameError | InvalidTodoDescriptionError | InvalidTodoStatusError, ITodoInserted>> {
     const todoInstance = Todo.create(todo)
     if (todoInstance.isLeft()) {
-      return left(null)
+      return left(todoInstance.value)
     }
     const createdId = await this.todoRepository.create(todoInstance.value)
     const result = await this.todoRepository.findById(createdId)
