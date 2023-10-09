35542aa8bd1e1e3da3ffd8d053c976abe1ec5424
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-09 19:38:05 -0300

    feat(usecase): adiciona findtodobyid use case

---
+++ backend-with-ca/src/usecases/find-todo-by-id/find-todo-by-id.ts
@@ -0,0 +1,22 @@
+import { left, type Either, right } from '../../shared/either'
+import { TodoNotFoundError } from '../create-new-todo/errors/todo-not-found-error'
+import { type ITodoInserted } from '../create-new-todo/interfaces/todo-inserted'
+import { type ITodoRepository } from '../create-new-todo/ports/todo-repository'
+import { type IUseCase } from '../shared/ports/use-case'
+
+class FindTodoByIdUseCase implements IUseCase {
+  private readonly todoRepository: ITodoRepository
+  constructor (todoRepository: ITodoRepository) {
+    this.todoRepository = todoRepository
+  }
+
+  public async execute (todoId: string): Promise<Either<TodoNotFoundError, ITodoInserted>> {
+    const findResult = await this.todoRepository.findById(todoId)
+    if (!findResult) {
+      return left(new TodoNotFoundError(todoId))
+    }
+    return right(findResult)
+  }
+}
+
+export { FindTodoByIdUseCase }
