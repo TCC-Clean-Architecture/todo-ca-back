76ca9534a45cca5a11a58e29296d5959dc67aaa1
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 22:46:54 -0300

    test(todo): implementa teste de todo not found

--- backend-with-ca/src/tests/usecases/create-new-todo.test.ts
+++ backend-with-ca/src/tests/usecases/create-new-todo.test.ts
@@ -4,6 +4,8 @@ import { CreateNewTodoUseCase } from '../../usecases/create-new-todo/create-new-
 import { type ITodo } from '../../entities/interfaces/todo'
 import { InMemoryTodoRepository } from '../../usecases/create-new-todo/repository/in-memory-todo-repository'
 import { InvalidTodoNameError } from '../../entities/errors/invalid-name-error'
+import { type ITodoRepository } from '../../usecases/create-new-todo/ports/todo-repository'
+import { TodoNotFoundError } from '../../usecases/errors/todo-not-found-error'
 
 describe('Create new todo', () => {
   it('should create a new todo', async () => {
@@ -18,7 +20,6 @@ describe('Create new todo', () => {
     expect(result.isRight()).to.equal(true)
     expect(result.value).to.deep.include(todo)
   })
-
   it('should return an error on creating new todo', async () => {
     const todo: ITodo = {
       name: 'a',
@@ -30,5 +31,20 @@ describe('Create new todo', () => {
     const result = await createNewTodoUseCase.execute(todo)
     expect(result.isLeft()).to.equal(true)
     expect(result.value).to.instanceOf(InvalidTodoNameError)
+  })
+  it('should not find the inserted todo', async () => {
+    const todo: ITodo = {
+      name: 'thisistodo',
+      description: 'thisisdescription',
+      status: 'todo'
+    }
+    const todoRepositoryMock: ITodoRepository = {
+      create: async () => 'abc',
+      findById: async () => null
+    }
+    const createNewTodoUseCase = new CreateNewTodoUseCase(todoRepositoryMock)
+    const result = await createNewTodoUseCase.execute(todo)
+    expect(result.isLeft()).to.equal(true)
+    expect(result.value).to.be.instanceOf(TodoNotFoundError)
   })
 })
