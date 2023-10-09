62030dd1b9382caf4deb9f38b14886a7a4d8e93f
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 22:27:00 -0300

    test(todo): implementa teste validando tipos de erro no use case

--- backend-with-ca/src/tests/usecases/create-new-todo.test.ts
+++ backend-with-ca/src/tests/usecases/create-new-todo.test.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai'
 import { CreateNewTodoUseCase } from '../../usecases/create-new-todo/create-new-todo'
 import { type ITodo } from '../../entities/interfaces/todo'
 import { InMemoryTodoRepository } from '../../usecases/create-new-todo/repository/in-memory-todo-repository'
+import { InvalidTodoNameError } from '../../entities/errors/invalid-name-error'
 
 describe('Create new todo', () => {
   it('should create a new todo', async () => {
@@ -16,5 +17,18 @@ describe('Create new todo', () => {
     const result = await createNewTodoUseCase.execute(todo)
     expect(result.isRight()).to.equal(true)
     expect(result.value).to.deep.include(todo)
+  })
+
+  it('should return an error on creating new todo', async () => {
+    const todo: ITodo = {
+      name: 'a',
+      description: 'thisisdescription',
+      status: 'todo'
+    }
+    const todoRepository = new InMemoryTodoRepository([])
+    const createNewTodoUseCase = new CreateNewTodoUseCase(todoRepository)
+    const result = await createNewTodoUseCase.execute(todo)
+    expect(result.isLeft()).to.equal(true)
+    expect(result.value).to.instanceOf(InvalidTodoNameError)
   })
 })
