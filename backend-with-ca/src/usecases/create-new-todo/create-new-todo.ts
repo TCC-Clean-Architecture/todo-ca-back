b3914e2932d812469a9b738f89cdc0011ed94ab5
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-08 22:20:19 -0300

    feat(todo): implementa factory de todo no caso de uso

--- backend-with-ca/src/usecases/create-new-todo/create-new-todo.ts
+++ backend-with-ca/src/usecases/create-new-todo/create-new-todo.ts
@@ -2,6 +2,7 @@ import { type Either, right, left } from '../../shared/either'
 import { type ITodo } from '../../entities/interfaces/todo'
 import { type ITodoInserted } from './interfaces/todo-inserted'
 import { type ITodoRepository } from './ports/todo-repository'
+import { Todo } from '../../entities/todo'
 
 class CreateNewTodoUseCase {
   private readonly todoRepository: ITodoRepository
@@ -10,7 +11,11 @@ class CreateNewTodoUseCase {
   }
 
   public async execute (todo: ITodo): Promise<Either<null, ITodoInserted>> {
-    const createdId = await this.todoRepository.create(todo)
+    const todoInstance = Todo.create(todo)
+    if (todoInstance.isLeft()) {
+      return left(null)
+    }
+    const createdId = await this.todoRepository.create(todoInstance.value)
     const result = await this.todoRepository.findById(createdId)
     if (!createdId || !result) {
       return left(null)
