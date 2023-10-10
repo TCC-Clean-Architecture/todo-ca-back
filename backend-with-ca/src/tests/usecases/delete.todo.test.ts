3ccddb251bf0baf82cf76dbbebef299a1832c31c
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-09 23:00:44 -0300

    feat(delete): adiciona validacao de delete

--- backend-with-ca/src/tests/usecases/delete.todo.test.ts
+++ backend-with-ca/src/tests/usecases/delete.todo.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import { InMemoryTodoRepository } from '../../usecases/shared/repository/in-memory-todo-repository'
 import { type ITodoWithId } from '../../entities/interfaces/todo'
 import { DeleteTodoUseCase } from '../../usecases/delete-todo/delete-todo'
+import { TodoNotFoundError } from '../../usecases/create-new-todo/errors/todo-not-found-error'
 
 describe('Delete todo use case testing', () => {
   it('should delete todo', async () => {
@@ -17,5 +18,12 @@ describe('Delete todo use case testing', () => {
     const result = await useCaseInstance.execute(todo.id)
     expect(result.value).to.equal(todo.id)
     expect(result.isRight()).to.equal(true)
+  })
+  it('should not find todo to delete', async () => {
+    const todoRepository = new InMemoryTodoRepository([])
+    const useCaseInstance = new DeleteTodoUseCase(todoRepository)
+    const result = await useCaseInstance.execute('abcde')
+    expect(result.value).to.be.instanceOf(TodoNotFoundError)
+    expect(result.isLeft()).to.equal(true)
   })
 })
--- backend-with-ca/src/usecases/delete-todo/delete-todo.ts
+++ backend-with-ca/src/usecases/delete-todo/delete-todo.ts
@@ -1,4 +1,5 @@
-import { type Either, right } from '../../shared/either'
+import { type Either, right, left } from '../../shared/either'
+import { TodoNotFoundError } from '../create-new-todo/errors/todo-not-found-error'
 import { type ITodoRepository } from '../shared/ports/todo-repository'
 import { type IUseCase } from '../shared/ports/use-case'
 
@@ -8,7 +9,11 @@ class DeleteTodoUseCase implements IUseCase {
     this.todoRepository = todoRepository
   }
 
-  public async execute (todoId: string): Promise<Either<void, string>> {
+  public async execute (todoId: string): Promise<Either<TodoNotFoundError, string>> {
+    const todoExists = await this.todoRepository.findById(todoId)
+    if (!todoExists) {
+      return left(new TodoNotFoundError(todoId))
+    }
     const result = await this.todoRepository.delete(todoId)
     return right(result)
   }
