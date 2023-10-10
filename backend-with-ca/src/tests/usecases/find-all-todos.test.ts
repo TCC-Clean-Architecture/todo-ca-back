df8b43cf288658e89dc42148eac0f568ccdc7e5f
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-09 22:12:31 -0300

    feat(usecase): adiciona caso de uso de buscar todos todos

--- backend-with-ca/src/tests/usecases/find-all-todos.test.ts
+++ backend-with-ca/src/tests/usecases/find-all-todos.test.ts
@@ -1,12 +1,28 @@
 import { expect } from 'chai'
 import { FindAllTodoUseCase } from '../../usecases/find-all-todos/find-all-todos'
 import { InMemoryTodoRepository } from '../../usecases/shared/repository/in-memory-todo-repository'
+import { type ITodoWithId } from '../../entities/interfaces/todo'
 
 describe('Find all todo use case testing', () => {
-  it('should list empty list of todos', () => {
+  it('should return all todos inserted', async () => {
+    const todos: ITodoWithId = {
+      id: 'id',
+      name: 'name',
+      description: 'description',
+      status: 'todo',
+      createdAt: new Date()
+    }
+    const todoRepository = new InMemoryTodoRepository([todos, todos])
+    const useCaseInstance = new FindAllTodoUseCase(todoRepository)
+    const result = await useCaseInstance.execute()
+    expect(result.value).to.deep.equal([todos, todos])
+    expect(result.isRight()).to.equal(true)
+  })
+  it('should list empty list of todos', async () => {
     const todoRepository = new InMemoryTodoRepository([])
     const useCaseInstance = new FindAllTodoUseCase(todoRepository)
-    const result = useCaseInstance.execute()
-    expect(result).to.deep.equal([])
+    const result = await useCaseInstance.execute()
+    expect(result.value).to.deep.equal([])
+    expect(result.isRight()).to.equal(true)
   })
 })
---
+++ backend-with-ca/src/usecases/find-all-todos/find-all-todos.ts
@@ -0,0 +1,18 @@
+import { type ITodoWithId } from '../../entities/interfaces/todo'
+import { type Either, right } from '../../shared/either'
+import { type ITodoRepository } from '../shared/ports/todo-repository'
+import { type IUseCase } from '../shared/ports/use-case'
+
+class FindAllTodoUseCase implements IUseCase {
+  private readonly todoRepository: ITodoRepository
+  constructor (todoRepository: ITodoRepository) {
+    this.todoRepository = todoRepository
+  }
+
+  public async execute (): Promise<Either<void, ITodoWithId[]>> {
+    const result = await this.todoRepository.findAll()
+    return right(result)
+  }
+}
+
+export { FindAllTodoUseCase }
