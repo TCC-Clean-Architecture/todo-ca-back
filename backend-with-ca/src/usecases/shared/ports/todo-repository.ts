d2dd14a5a34f170568de1413bae58a1f98703fce
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-09 23:16:43 -0300

    feat(repository): adiciona update no repository

--- backend-with-ca/src/usecases/shared/ports/todo-repository.ts
+++ backend-with-ca/src/usecases/shared/ports/todo-repository.ts
@@ -1,8 +1,9 @@
-import { type ICompleteTodo, type ITodoWithId } from '../../../entities/interfaces/todo'
+import { type ITodo, type ICompleteTodo, type ITodoWithId } from '../../../entities/interfaces/todo'
 
 export interface ITodoRepository {
   create: (todo: ICompleteTodo) => Promise<string>
   findById: (todoId: string) => Promise<ITodoWithId | null>
   findAll: () => Promise<ITodoWithId[]>
   delete: (todoId: string) => Promise<string>
+  update: (todoId: string, content: Partial<ITodo>) => Promise<ITodoWithId | null>
 }
