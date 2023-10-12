86bd292bdce9128164752c2508cd42a06453781f
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-12 11:19:34 -0300

    feat(interface): adiciona interface de requst com body e params

--- backend-with-ca/src/web-controllers/port/http-request.ts
+++ backend-with-ca/src/web-controllers/port/http-request.ts
@@ -1,7 +1,12 @@
-export interface IHttpRequestWithBody<T = object> {
-  body: T
+export interface IHttpRequestWithBody<B = object> {
+  body: B
 }
 
-export interface IHttpRequestWithParams<T = object> {
-  params: T
+export interface IHttpRequestWithParams<P = object> {
+  params: P
+}
+
+export interface IHttpRequestWithBodyAndParams<B = object, P = object> {
+  body: B
+  params: P
 }
