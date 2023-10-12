744786ce34041ad61292b30230384551c38df900
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-12 10:18:37 -0300

    feat(controller): adiciona controller de find todo by id

---
+++ backend-with-ca/src/tests/web-controllers/find-specific-todo-controller.test.ts
@@ -0,0 +1,45 @@
+import { expect } from 'chai'
+import { type ITodoWithId } from '../../entities/interfaces/todo'
+import { InMemoryTodoRepository } from '../../usecases/shared/repository/in-memory-todo-repository'
+import { type IHttpRequestWithParams } from '../../web-controllers/port/http-request'
+import { FindTodoByIdUseCase } from '../../usecases/find-todo-by-id/find-todo-by-id'
+import { todoFixture } from '../helper/fixtures/todo-fixture'
+import { FindSpecificTodoController } from '../../web-controllers/find-specific-todo-controller'
+
+describe('FindSpecificTodoController implementation testing', () => {
+  it('should find an specific todo by id and return success', async () => {
+    const fakeTodo = todoFixture()
+    const todoRepository = new InMemoryTodoRepository([fakeTodo])
+    const findTodoByIdUseCase = new FindTodoByIdUseCase(todoRepository)
+    const controllerInstance = new FindSpecificTodoController(findTodoByIdUseCase)
+    const request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>> = {
+      params: {
+        id: fakeTodo.id
+      }
+    }
+    const response = await controllerInstance.handler(request)
+    expect(response.description).to.equal('Todo found successfully')
+    expect(response.statusCode).to.equal(200)
+    expect(response.message).to.equal('OK')
+    expect(response.type).to.equal('success')
+    expect(response.content).to.deep.equal(fakeTodo)
+  })
+  it('should not find todo and return error', async () => {
+    const todoRepository = new InMemoryTodoRepository([])
+    const findTodoByIdUseCase = new FindTodoByIdUseCase(todoRepository)
+    const controllerInstance = new FindSpecificTodoController(findTodoByIdUseCase)
+    const request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>> = {
+      params: {
+        id: 'todo'
+      }
+    }
+    const response = await controllerInstance.handler(request)
+    expect(response.description).to.equal('Error on get todo by id')
+    expect(response.statusCode).to.equal(400)
+    expect(response.message).to.equal('Bad Request')
+    expect(response.type).to.equal('error')
+    expect(response.content).to.deep.equal({
+      message: 'Todo not found: todo.'
+    })
+  })
+})
---
+++ backend-with-ca/src/web-controllers/find-specific-todo-controller.ts
@@ -0,0 +1,33 @@
+import { type ITodoWithId } from '../entities/interfaces/todo'
+import { type FindTodoByIdUseCase } from '../usecases/find-todo-by-id/find-todo-by-id'
+import { badRequest, ok } from './helper/http-response-builder'
+import { type Controller } from './port/controller'
+import { type IHttpRequestWithParams } from './port/http-request'
+import { type IHttpResponse } from './port/http-response'
+
+class FindSpecificTodoController implements Controller {
+  private readonly useCase: FindTodoByIdUseCase
+  constructor (useCase: FindTodoByIdUseCase) {
+    this.useCase = useCase
+  }
+
+  async handler (request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>>): Promise<IHttpResponse> {
+    const response = await this.useCase.execute(request.params.id)
+    if (response.isLeft()) {
+      return badRequest({
+        description: 'Error on get todo by id',
+        content: {
+          message: response.value.message
+        }
+      })
+    }
+    return ok({
+      description: 'Todo found successfully',
+      content: response.value
+    })
+  }
+}
+
+export {
+  FindSpecificTodoController
+}
