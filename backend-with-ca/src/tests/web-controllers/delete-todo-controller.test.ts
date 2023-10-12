afa48b422207501ea5158a4f59414a35c9d06030
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-12 11:14:41 -0300

    feat(controller): adiciona controller de delete

---
+++ backend-with-ca/src/tests/web-controllers/delete-todo-controller.test.ts
@@ -0,0 +1,47 @@
+import { expect } from 'chai'
+import { type ITodoWithId } from '../../entities/interfaces/todo'
+import { InMemoryTodoRepository } from '../../usecases/shared/repository/in-memory-todo-repository'
+import { type IHttpRequestWithParams } from '../../web-controllers/port/http-request'
+import { todoFixture } from '../helper/fixtures/todo-fixture'
+import { DeleteTodoUseCase } from '../../usecases/delete-todo/delete-todo'
+import { DeleteTodoController } from '../../web-controllers/delete-todo-controller'
+
+describe('DeleteTodoController implementation testing', () => {
+  it('should find an specific todo by id and return success', async () => {
+    const fakeTodo = todoFixture()
+    const todoRepository = new InMemoryTodoRepository([fakeTodo])
+    const deleteTodoUseCase = new DeleteTodoUseCase(todoRepository)
+    const controllerInstance = new DeleteTodoController(deleteTodoUseCase)
+    const request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>> = {
+      params: {
+        id: fakeTodo.id
+      }
+    }
+    const response = await controllerInstance.handler(request)
+    expect(response.description).to.equal('Todo deleted successfully')
+    expect(response.statusCode).to.equal(200)
+    expect(response.message).to.equal('OK')
+    expect(response.type).to.equal('success')
+    expect(response.content).to.deep.equal({
+      id: fakeTodo.id
+    })
+  })
+  it('should not find todo and return error', async () => {
+    const todoRepository = new InMemoryTodoRepository([])
+    const deleteTodoUseCase = new DeleteTodoUseCase(todoRepository)
+    const controllerInstance = new DeleteTodoController(deleteTodoUseCase)
+    const request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>> = {
+      params: {
+        id: 'todo'
+      }
+    }
+    const response = await controllerInstance.handler(request)
+    expect(response.description).to.equal('Error on delete todo')
+    expect(response.statusCode).to.equal(400)
+    expect(response.message).to.equal('Bad Request')
+    expect(response.type).to.equal('error')
+    expect(response.content).to.deep.equal({
+      message: 'Todo not found: todo.'
+    })
+  })
+})
---
+++ backend-with-ca/src/web-controllers/delete-todo-controller.ts
@@ -0,0 +1,35 @@
+import { type ITodoWithId } from '../entities/interfaces/todo'
+import { type DeleteTodoUseCase } from '../usecases/delete-todo/delete-todo'
+import { badRequest, ok } from './helper/http-response-builder'
+import { type Controller } from './port/controller'
+import { type IHttpRequestWithParams } from './port/http-request'
+import { type IHttpResponse } from './port/http-response'
+
+class DeleteTodoController implements Controller {
+  private readonly useCase: DeleteTodoUseCase
+  constructor (useCase: DeleteTodoUseCase) {
+    this.useCase = useCase
+  }
+
+  async handler (request: IHttpRequestWithParams<Pick<ITodoWithId, 'id'>>): Promise<IHttpResponse> {
+    const response = await this.useCase.execute(request.params.id)
+    if (response.isLeft()) {
+      return badRequest({
+        description: 'Error on delete todo',
+        content: {
+          message: response.value.message
+        }
+      })
+    }
+    return ok({
+      description: 'Todo deleted successfully',
+      content: {
+        id: response.value
+      }
+    })
+  }
+}
+
+export {
+  DeleteTodoController
+}
