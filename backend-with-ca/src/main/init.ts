90aea2e6172d509744d8a61f035f673b0dbf5a83
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-14 20:37:36 -0300

    feat(cors&env): adiciona cors e dotenv

--- backend-with-ca/src/main/init.ts
+++ backend-with-ca/src/main/init.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv'
+
 import { connectDatabase } from '@/main/configs/mongodb'
 
 const init = async (): Promise<void> => {
+  dotenv.config()
   await connectDatabase()
 }
 
