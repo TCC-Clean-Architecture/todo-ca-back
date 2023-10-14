9a4dec2d807d4716126b67793cce8ca5847c3b9f
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-14 10:58:03 -0300

    style(todo-repository): altera interface

--- backend-with-ca/src/shared/todo-repository.ts
+++ backend-with-ca/src/shared/todo-repository.ts
@@ -1,9 +1,9 @@
 import { type ICompleteTodo, type ITodo, type ITodoWithId } from '@/entities/interfaces/todo'
 
 export interface ITodoRepository {
-  create: (todo: ICompleteTodo) => Promise<string>
-  findById: (todoId: string) => Promise<ITodoWithId | null>
-  findAll: () => Promise<ITodoWithId[]>
-  delete: (todoId: string) => Promise<string>
-  update: (todoId: string, content: Partial<ITodo>) => Promise<string | null>
+  create (todo: ICompleteTodo): Promise<string>
+  findById (todoId: string): Promise<ITodoWithId | null>
+  findAll (): Promise<ITodoWithId[]>
+  delete (todoId: string): Promise<string>
+  update (todoId: string, content: Partial<ITodo>): Promise<string | null>
 }
