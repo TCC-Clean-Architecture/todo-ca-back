f09b1f284146740eccc755f1ba97525839114601
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-16 23:35:18 -0300

    feat(usecase): adiciona find todo by id

--- backend-with-ca/src/usecases/todo/find-todo-by-id/find-todo-by-id.ts
+++ backend-with-ca/src/usecases/todo/find-todo-by-id/find-todo-by-id.ts
@@ -1,23 +1,30 @@
 import { type ITodoWithId } from '@/entities/interfaces/todo'
+import { TodosEmbedded } from '@/entities/todo-list/todos-embedded'
 import { type Either, left, right } from '@/shared/either'
 import { UnexpectedError } from '@/shared/errors/unexpected-error'
-import { type ITodoRepository } from '@/shared/todo-repository'
+import { type ITodoListRepository } from '@/shared/todo-list-repository'
 import { type IUseCase } from '@/usecases/shared/ports/use-case'
 import { TodoNotFoundError } from '@/usecases/todo/shared/errors/todo-not-found-error'
+import { TodoListNotFoundError } from '@/usecases/todo-list/shared/errors/todo-list-not-found-error'
 
 class FindTodoByIdUseCase implements IUseCase {
-  private readonly todoRepository: ITodoRepository
-  constructor (todoRepository: ITodoRepository) {
-    this.todoRepository = todoRepository
+  private readonly todoListRepository: ITodoListRepository
+  constructor (todoListRepository: ITodoListRepository) {
+    this.todoListRepository = todoListRepository
   }
 
-  public async execute (todoId: string): Promise<Either<TodoNotFoundError | UnexpectedError, ITodoWithId>> {
+  public async execute (todoId: string, listId: string): Promise<Either<TodoListNotFoundError | TodoNotFoundError | UnexpectedError, ITodoWithId>> {
     try {
-      const findResult = await this.todoRepository.findById(todoId)
-      if (!findResult) {
+      const list = await this.todoListRepository.findById(listId)
+      if (!list) {
+        return left(new TodoListNotFoundError(listId))
+      }
+      const todos = new TodosEmbedded(list.todos)
+      const result = todos.findById(todoId)
+      if (result.isLeft()) {
         return left(new TodoNotFoundError(todoId))
       }
-      return right(findResult)
+      return right(result.value)
     } catch (err) {
       return left(new UnexpectedError('Something went wrong on attempt to find todo by id'))
     }
