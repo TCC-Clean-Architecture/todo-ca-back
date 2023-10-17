898f53c9cc2b485aade36dd8be7407fd7c122fd0
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-16 23:22:35 -0300

    feat(controller): adiciona controller de find all

--- backend-with-ca/src/web-controllers/todo/find-all-todo-controller.ts
+++ backend-with-ca/src/web-controllers/todo/find-all-todo-controller.ts
@@ -1,9 +1,12 @@
 import { type ITodoWithId } from '@/entities/interfaces/todo'
+import { type IListId } from '@/entities/interfaces/todo-list'
 import { type FindAllTodoUseCase } from '@/usecases/todo/find-all-todos/find-all-todos'
 import { badRequest, ok } from '@/web-controllers/helper/http-response-builder'
 import { idConverter } from '@/web-controllers/helper/id-property-name-converter'
 import { type Controller } from '@/web-controllers/port/controller'
 import { type IHttpResponse } from '@/web-controllers/port/http-response'
+
+import { type IHttpRequestWithParams } from '../port/http-request'
 
 class FindAllTodoController implements Controller {
   private readonly useCase: FindAllTodoUseCase
@@ -11,8 +14,8 @@ class FindAllTodoController implements Controller {
     this.useCase = useCase
   }
 
-  async handler (): Promise<IHttpResponse<ITodoWithId[] | object>> {
-    const response = await this.useCase.execute()
+  async handler (request: IHttpRequestWithParams<IListId>): Promise<IHttpResponse<ITodoWithId[] | object>> {
+    const response = await this.useCase.execute(request.params.listId)
     if (response.isLeft()) {
       return badRequest({
         description: 'Error on find all todos',
