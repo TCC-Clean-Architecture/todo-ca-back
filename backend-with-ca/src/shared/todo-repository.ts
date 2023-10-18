b0cd06f8c8d3295445fe82ccc025fe4b31d88d78
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-17 22:23:51 -0300

    refactor(unused): remove codigos nao utilizados

--- backend-with-ca/src/shared/todo-repository.ts
+++
@@ -1,9 +0,0 @@
-import { type ICompleteTodo, type ITodo, type ITodoWithId } from '@/entities/interfaces/todo'
-
-export interface ITodoRepository {
-  create (todo: ICompleteTodo): Promise<string>
-  findById (todoId: string): Promise<ITodoWithId | null>
-  findAll (): Promise<ITodoWithId[]>
-  delete (todoId: string): Promise<string | null>
-  update (todoId: string, content: Partial<ITodo>): Promise<string | null>
-}
--- backend-with-ca/src/usecases/todo-list/update-todo-list/errors/todo-list-update-error.ts
+++
@@ -1,6 +0,0 @@
-export class TodoListUpdateError extends Error {
-  public readonly name = 'TodoListUpdateError'
-  constructor (todoListId: string) {
-    super('Todo list update error: ' + todoListId + '.')
-  }
-}
