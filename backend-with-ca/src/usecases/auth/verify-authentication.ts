ea32b7dc66c4b29023e92df8c0ae983960d72549
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-19 02:13:57 -0300

    feat(usecase): adiciona verify authentication use case

---
+++ backend-with-ca/src/usecases/auth/verify-authentication.ts
@@ -0,0 +1,22 @@
+import { InvalidTokenError } from '@/external/security/error/InvalidTokenError'
+import { type Either, left, right } from '@/shared/either'
+import { type IJwtProvider, type ITokenPayload } from '@/shared/security-repository'
+
+import { type IUseCase } from '../shared/ports/use-case'
+
+class VerifyAuthenticationUseCase implements IUseCase {
+  private readonly jwtProvider: IJwtProvider
+  constructor (jwtProvider: IJwtProvider) {
+    this.jwtProvider = jwtProvider
+  }
+
+  async execute (token: string): Promise<Either<InvalidTokenError, ITokenPayload>> {
+    const verifyResult = this.jwtProvider.verify(token)
+    if (verifyResult.isLeft()) {
+      return left(new InvalidTokenError())
+    }
+    return right(verifyResult.value)
+  }
+}
+
+export { VerifyAuthenticationUseCase }
