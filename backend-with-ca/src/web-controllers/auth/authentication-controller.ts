cfab9b439b52ae2b287d66224b15cad877f92a2c
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-19 00:24:08 -0300

    feat(controller): adiciona controller de autenticacao

--- backend-with-ca/src/web-controllers/auth/authentication-controller.ts
+++ backend-with-ca/src/web-controllers/auth/authentication-controller.ts
@@ -1,5 +1,5 @@
-import { type IUser } from '@/entities/interfaces/user'
-import { type CreateUserUseCase } from '@/usecases/user/create-user'
+import { type IUserAuth } from '@/entities/interfaces/user'
+import { type AuthenticationUseCase } from '@/usecases/auth/authentication'
 
 import { badRequest, ok } from '../helper/http-response-builder'
 import { idConverter } from '../helper/id-property-name-converter'
@@ -7,27 +7,27 @@ import { type Controller } from '../port/controller'
 import { type IHttpRequestWithBody } from '../port/http-request'
 import { type IHttpResponse } from '../port/http-response'
 
-class CreateUserController implements Controller {
-  private readonly useCase: CreateUserUseCase
-  constructor (useCase: CreateUserUseCase) {
+class AuthenticationController implements Controller {
+  private readonly useCase: AuthenticationUseCase
+  constructor (useCase: AuthenticationUseCase) {
     this.useCase = useCase
   }
 
-  async handler (request: IHttpRequestWithBody<IUser>): Promise<IHttpResponse> {
+  async handler (request: IHttpRequestWithBody<IUserAuth>): Promise<IHttpResponse> {
     const response = await this.useCase.execute(request.body)
     if (response.isLeft()) {
       return badRequest({
-        description: 'Error on create user',
+        description: 'Error on authenticate user',
         content: {
           message: response.value.message
         }
       })
     }
     return ok({
-      description: 'User created successfully',
+      description: 'User authenticated successfully',
       content: idConverter(response.value)
     })
   }
 }
 
-export { CreateUserController }
+export { AuthenticationController }
