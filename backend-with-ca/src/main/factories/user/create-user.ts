bcc01f2a61cae3049af0567469d1212111f4b3b6
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-19 02:52:56 -0300

    feat(factory): adiciona factories

--- backend-with-ca/src/main/factories/user/create-user.ts
+++ backend-with-ca/src/main/factories/user/create-user.ts
@@ -1,8 +1,14 @@
+import { MongoUserRepository } from '@/external/repositories/mongo-user-repository'
+import { Bcrypt } from '@/external/security/bcrypt'
 import { CreateUserUseCase } from '@/usecases/user/create-user'
 import { CreateUserController } from '@/web-controllers/user/create-user-controller'
 
 const makeCreateUserController = (): CreateUserController => {
-  const repository = Mongo
-  const useCase = CreateUserUseCase()
-  const controller = CreateUserController()
+  const repository = new MongoUserRepository()
+  const hashProvider = new Bcrypt()
+  const useCase = new CreateUserUseCase(repository, hashProvider)
+  const controller = new CreateUserController(useCase)
+  return controller
 }
+
+export { makeCreateUserController }
