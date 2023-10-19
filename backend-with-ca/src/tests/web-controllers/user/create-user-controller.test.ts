57791d192ec8c7f0715fb21a8fc1d44e885167c7
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2023-10-18 21:39:44 -0300

    feat(controller): adiciona controller de user

---
+++ backend-with-ca/src/tests/web-controllers/user/create-user-controller.test.ts
@@ -0,0 +1,46 @@
+import { expect } from 'chai'
+
+import { type IUser } from '@/entities/interfaces/user'
+import { InMemoryUserRepository } from '@/usecases/shared/repository/in-memory-user-repository'
+import { CreateUserUseCase } from '@/usecases/user/create-user'
+import { CreateUserController } from '@/web-controllers/user/create-user-controller'
+
+describe('Create user controller testing', () => {
+  it('should return success', async () => {
+    const user: IUser = {
+      email: 'email@email.com',
+      password: 'Password100'
+    }
+    const repository = new InMemoryUserRepository([])
+    const useCase = new CreateUserUseCase(repository)
+    const controller = new CreateUserController(useCase)
+    const response = await controller.handler({
+      body: user
+    })
+    expect(response.description).to.equal('User created successfully')
+    expect(response.statusCode).to.equal(200)
+    expect(response.message).to.equal('OK')
+    expect(response.type).to.equal('success')
+    expect(response.content).to.deep.include(user)
+  })
+  it('should return fail', async () => {
+    const user: IUser = {
+      email: 'emailemail.com',
+      password: 'Password100'
+    }
+    const repository = new InMemoryUserRepository([])
+    const useCase = new CreateUserUseCase(repository)
+    const controller = new CreateUserController(useCase)
+    const response = await controller.handler({
+      body: user
+    })
+    console.log(response.content)
+    expect(response.description).to.equal('Error on create user')
+    expect(response.statusCode).to.equal(400)
+    expect(response.message).to.equal('Bad Request')
+    expect(response.type).to.equal('error')
+    expect(response.content).to.deep.equal({
+      message: 'Invalid email: emailemail.com.'
+    })
+  })
+})
